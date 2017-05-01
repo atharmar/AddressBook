77092e7b19a8ec18b5578bd61f6d92103b917abe
atharmar <Aruna.Tharmar@bcbsnc.com>
2017-05-01 11:14:21 -0400

    changes from 5/1

---
+++ AddressBook/MenuOption.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public enum MenuOption
+    {
+        Exit,
+        AddPerson,
+        AddCompany,
+        ListContacts,
+        SearchContacts,
+        RemoveContact,
+        AddRecipe,
+        ListRecipes,
+        SearchEverything,
+
+        UPPER_LIMIT
+    }
+}
