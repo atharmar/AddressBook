bac38a9dbb2999ea5c9c26af049b997dc7ff06df
atharmar <Aruna.Tharmar@bcbsnc.com>
2017-05-02 16:01:30 -0400

    Updated with Dapper

---
+++ AddressBook.Tests/RolodexTests.cs
@@ -0,0 +1,82 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook.Tests
+{
+    [TestFixture]
+    public class RolodexTests
+    {
+        private IGetInputFromUsers _input;
+        private IHandleContacts _contacts;
+        private IHandleRecipes _recipes;
+        private Rolodex _rolodex;
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            //Mock Objects//
+            _input = Substitute.For<IGetInputFromUsers>();
+            _contacts = Substitute.For<IHandleContacts>();
+            _recipes = Substitute.For<IHandleRecipes>();
+            _rolodex = new Rolodex(_contacts, _recipes, _input);
+
+        }
+        [Test]
+        public void ExitJustDoesNothing()
+        {
+            //Arrange
+             _input.GetNumber().Returns(0);
+
+            //Act
+            _rolodex.DoStuff();
+
+            //Assert
+            _input.Received().GetNumber();
+            _contacts.DidNotReceive().GetAllContacts();
+            _recipes.DidNotReceive().GetAllRecipes();
+            _contacts.DidNotReceiveWithAnyArgs().CreateCompany(null, null);
+        }
+        [Test]
+        public void AddPersonAddsAPersonJustLikeYouWouldExpectItTo()
+        {
+            //Arrange
+            
+            _input.GetNumber().Returns(1,0);
+            _input.GetNonEmptyString().Returns("Bob", "Marley", "567-678-6878");           
+
+            //Act
+            _rolodex.DoStuff();
+
+            //Assert ( make sure the method calls with these reports) //
+            _input.Received(2).GetNumber();
+            _contacts.DidNotReceive().GetAllContacts();
+            _recipes.DidNotReceive().GetAllRecipes();
+            _contacts.DidNotReceiveWithAnyArgs().CreateCompany(null, null);
+            _contacts.Received().CreatePerson("Bob", "Marley", "567-678-6878");
+        }
+        [Test]
+        public void AddRecipeAddsARecipeJustLikeYouWouldExpectItTo()
+        {
+            //Arrange
+
+            _input.GetNumber().Returns(6,1,0);
+            _input.GetNonEmptyString().Returns("Chicken Alfredo");
+
+            //Act
+            _rolodex.DoStuff();
+
+            //Assert ( make sure the method calls with these reports) //
+            _input.Received(3).GetNumber();
+            _contacts.DidNotReceive().GetAllContacts();
+            _recipes.DidNotReceive().GetAllRecipes();
+            _contacts.DidNotReceiveWithAnyArgs().CreateCompany(null, null);
+            _contacts.DidNotReceiveWithAnyArgs().CreatePerson(null, null, null);
+            _recipes.Received().Create("Chicken Alfredo", RecipeType.Entreés);
+        }
+    }
+}
--- AddressBook/Recipe.cs
+++ AddressBook/Recipe.cs
@@ -1,12 +1,31 @@
-﻿namespace AddressBook
+﻿using Dapper.Contrib.Extensions;
+
+namespace AddressBook
 {
     public class Recipe : IMatchable
     {
+        public Recipe() {} // using empty construtor for dapper//
         public Recipe(string title, RecipeType type)
         {
             _title = title;
             _type = type;
         }
+
+        [Key]
+        public int RecipeID { get; set; }
+
+        public string Name
+        {
+            get { return _title; } // string someTitle = recipe Title;
+            set { _title = value; }//recipe.Title = "Peanut Brittle?, value <- "peanut Brittle"
+        }
+
+        public RecipeType RecipeTypeId
+        {
+            get { return _type; }  
+            set { _type = value; } 
+        }
+
 
         public bool Matches(string term)
         {
@@ -19,6 +38,6 @@
         }
 
         private string _title;
-        private readonly RecipeType _type;
+        private RecipeType _type;
     }
 }
